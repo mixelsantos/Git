dcc4860b70e20b84affa52ae4ded6025e2ba524f
michel lima <mmichel.lima@hotmail.com.br>
2018-10-15 01:42:10 +0100

    Commite inicial 2

---
+++ Calendario/Calendario/Calendario/App.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamForms.Controls;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Calendario
+{
+    public partial class App : Application
+    {
+        Calendar calendar;
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            calendar = new Calendar
+            {
+                MaxDate = DateTime.Now.AddDays(30),
+                MinDate = DateTime.Now.AddDays(-1),
+                //DisableDatesLimitToMaxMinRange = true,
+                MultiSelectDates = false,
+                DisableAllDates = false,
+                WeekdaysShow = true,
+                ShowNumberOfWeek = true,
+                //BorderWidth = 1,
+                //BorderColor = Color.Transparent,
+                //OuterBorderWidth = 0,
+                //SelectedBorderWidth = 1,
+                ShowNumOfMonths = 1,
+                EnableTitleMonthYearView = true,
+                WeekdaysTextColor = Color.Teal,
+                StartDay = DayOfWeek.Monday,
+                SelectedTextColor = Color.Fuchsia,
+                SpecialDates = new List<SpecialDate>{
+                    new SpecialDate(DateTime.Now.AddDays(2)) { BackgroundColor = Color.Green, TextColor = Color.Accent, BorderColor = Color.Lime, BorderWidth=8, Selectable = true },
+                    new SpecialDate(DateTime.Now.AddDays(3))
+                    {
+                        BackgroundColor = Color.Green,
+                        TextColor = Color.Blue,
+                        Selectable = true,
+                        BackgroundPattern = new BackgroundPattern(1)
+                        {
+                            Pattern = new List<Pattern>
+                            {
+                                new Pattern{ WidthPercent = 1f, HightPercent = 0.25f, Color = Color.Yellow,Text = "Test", TextColor=Color.DarkBlue, TextSize=11, TextAlign=TextAlign.LeftTop},
+                                new Pattern{ WidthPercent = 1f, HightPercent = 0.25f, Color = Color.Red},
+                                new Pattern{ WidthPercent = 1f, HightPercent = 0.25f, Color = Color.Purple},
+                                //new Pattern{ WidthPercent = 1f, HightPercent = 0.25f, Color = Color.Green},
+                                new Pattern{ WidthPercent = 1f, HightPercent = 0.25f, Color = Color.Green, },
+                            }
+                        }
+                    },
+                    new SpecialDate(new DateTime(2018, 10, 20))
+                    {
+                        Selectable = true,
+                        TextColor = Color.Transparent,
+                        BackgroundImage = FileImageSource.FromFile("dcalendario.png") as FileImageSource,
+                        BackgroundColor = Color.Blue,
+                        BackgroundPattern = new BackgroundPattern(2)
+                        {
+                            
+                            Pattern = new List<Pattern>
+                            {
+                                new Pattern{ WidthPercent = 1f, HightPercent = 0.75f, Color = Color.Yellow,Text = "Test", TextColor=Color.DarkBlue, TextSize=11, TextAlign=TextAlign.LeftTop},                                
+                            }
+                        }
+
+                    },
+                    new SpecialDate(new DateTime(2018, 10, 27))
+                    {
+                        Selectable = true,
+                        TextColor = Color.Transparent,
+                        BackgroundImage = FileImageSource.FromFile("dcalendario.png") as FileImageSource,
+                        BackgroundColor = Color.Blue                        
+                    }
+                }
+            };
+
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                BackgroundColor = Color.White,
+                Content = new ScrollView
+                {
+                    Content = new StackLayout
+                    {
+                        Padding = new Thickness(5, Device.RuntimePlatform == Device.iOS ? 25 : 5, 5, 5),
+                        Children = {
+                            calendar//,c2
+						}
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Calendario/Calendario/Calendario/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using XamForms.Controls;
+
+namespace Calendario
+{
+    public partial class MainPage : ContentView
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnStart()
+        {
+            // Handle when your app starts
+            //calendar.SpecialDates.Add(new SpecialDate(DateTime.Now.AddDays(5)) { BackgroundColor = Color.Fuchsia, TextColor = Color.Accent, BorderColor = Color.Maroon, BorderWidth = 8 });
+            //calendar.SpecialDates.Add(new SpecialDate(DateTime.Now.AddDays(6)) { BackgroundColor = Color.Fuchsia, TextColor = Color.Accent, BorderColor = Color.Maroon, BorderWidth = 8 });
+            //calendar.RaiseSpecialDatesChanged();
+
+            var dates = new List<SpecialDate>();
+
+            var specialDate = new SpecialDate(new DateTime(2018, 10, 26));
+            specialDate.BackgroundColor = Color.Green;
+            specialDate.TextColor = Color.White;
+
+            dates.Add(specialDate);
+
+            calendario.SpecialDates = new ObservableCollection<SpecialDate>(dates);
+            calendario.SelectedDate = (DateTime.Now);
+
+        }
+    }
+}
